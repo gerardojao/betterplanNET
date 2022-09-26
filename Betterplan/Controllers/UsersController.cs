57e248740863c71cdd65d045d450498f1deec2a0
gerardojao <gerardojao@gmail.com>
2022-09-26 16:48:43 -0400

    Controller goals by USER

--- Betterplan/Controllers/UsersController.cs
+++ Betterplan/Controllers/UsersController.cs
@@ -36,23 +36,7 @@ namespace ApiBase.Controllers
         [HttpGet]
         public async Task<ActionResult> GetAllUsers()
         {
-            //Respuesta<object> respuesta = new();
-            //try
-            //{
-            //    var users = await _repository.SelectAll<User>();
-            //    foreach (var item in users)
-            //    {
-            //        respuesta.Data.Add(item);
-            //    }
-            //    respuesta.Ok = 1;
-            //    respuesta.Message = "Success";
-            //}
-            //catch (Exception e)
-            //{
-            //    respuesta.Ok = 0;
-            //    respuesta.Message = e.Message + " " + e.InnerException;
-            //}
-            //return Ok(respuesta);
+           
            var users =  await _repository.SelectAll<User>();
             return Ok(users);
         }
@@ -61,54 +45,67 @@ namespace ApiBase.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult> GetUserById(int id)
         {
-            //Respuesta<object> respuesta = new();
-            //try
-            //{
-            //    var user = await _context.user.Where(q => q.id == id).FirstOrDefaultAsync();
-            //    if (user == null)
-            //    {
-            //        respuesta.Ok = 0;
-            //        respuesta.Message = "User not found";
-
-            //    }
-            //    else
-            //    {
-            //        respuesta.Data.Add(new
-            //        {
-            //            user.firstname,
-            //            user.surname
-            //        });
-            //        respuesta.Ok = 1;
-            //        respuesta.Message = "Success";
-            //    }
-            //}
-            //catch (Exception e)
-            //{
-            //    respuesta.Ok = 0;
-            //    respuesta.Message = e.Message + " " + e.InnerException;
-            //}
-            //return Ok(respuesta);
+            
 
             var user = await _context.Users.Where(q => q.Id == id).FirstOrDefaultAsync();
-            //var advisor = await (from advisor in _context.Answers
-            //                     where advisor.QuestionId == question.Id
-            //                     select new
-            //                     {
-            //                         Id = answer.Id,
-            //                         Name = answer.Name,
-            //                         Image = answer.Image
-            //                     }).ToListAsync();
+    
             if (user != null)
             {
                 return Ok(new
                 {
                     user.Id,
-                    Name = user.Firstname +" "+ user.Surname,                    
+                    Name = user.Firstname +" "+ user.Surname,   
+                    user.Advisorid,
                     user.Created                   
                 });
             }
             return NotFound();
 
         }
+
+        [HttpGet("{id}/goal")]
+
+        public async Task<ActionResult> GetGoalByUserId(int id)
+        {
+           try
+            {
+                var goals = await _context.Goals.Where(q => q.Id == id).FirstOrDefaultAsync();
+                if (goals != null)
+                {
+                    var goalPortfolio = (from goal in _context.Goals
+                                         join port in _context.Portfolios on goal.Portfolioid equals port.Id
+                                         join ent in _context.Financialentities on port.Financialentityid equals ent.Id
+                                         where goals.Id == id
+                                         select new
+                                         {
+                                             goal.Title,
+                                             goal.Years,
+                                             goal.Initialinvestment,
+                                             goal.Monthlycontribution,
+                                             goal.Targetamount,
+                                             Portafolio = port.Title,
+                                             Entity = ent.Title,
+                                             goal.Created
+                                         }).ToList();
+               
+                    foreach (var gp in goalPortfolio)
+                    {
+                        return Ok(gp);
+                    }
+                   
+                }
+
+                return NotFound();
+            }
+
+            catch (Exception e)
+            {
+                return NotFound();
+
+            }
+           
+        }
+
+
     }
 }
