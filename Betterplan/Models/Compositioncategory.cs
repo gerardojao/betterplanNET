dc33bcb71f14ba2b8886bdb11518cfdf06ce7cb6
gerardojao <gerardojao@gmail.com>
2022-09-26 13:55:00 -0400

    scaffold database - getByID User

---
+++ Betterplan/Models/Compositioncategory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ApiBase.Models
+{
+    public partial class Compositioncategory
+    {
+        public Compositioncategory()
+        {
+            Compositionsubcategories = new HashSet<Compositionsubcategory>();
+        }
+
+        public string Name { get; set; }
+        public string Uuid { get; set; }
+        public int Id { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+
+        public virtual ICollection<Compositionsubcategory> Compositionsubcategories { get; set; }
+    }
+}
